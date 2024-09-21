b212c35b90b09f9031311030d21e8d6fd9222e31
drm <cheznl191065@gmail.com>
2024-09-20 23:47:19 -0600

    initial commit

---
+++ src/test/java/com/drm/arithmeticcalculator/ArithmeticCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drm.arithmeticcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
